b0e569aeb7170940de15a07de15a23babf0792ba
Federico <federico@mbp-di-federico.lan>
2013-12-05 14:37:06 +0100

    make of uhd drivers update with patches and latest version

--- ettus-uhd.rb
+++ ettus-uhd.rb
@@ -2,16 +2,20 @@ require 'formula'
 
 class EttusUhd < Formula
   homepage 'http://www.ettus.com/'
-  url 'https://github.com/EttusResearch/UHD-Mirror/archive/release_003_005_001.tar.gz'
-  sha1 '29066608d36aa25d9f1dbb64e41aa81e252b638a'
-  version '3.5.1'
-  head 'git://github.com/EttusResearch/UHD-Mirror.git'
+  url 'https://github.com/EttusResearch/uhd/archive/release_003_006_001.tar.gz'
+  sha1 'fc67e61a8b962f142ff208865a1dd503e25b7e0b'
+  version '3.6.1'
+  head 'git://github.com/EttusResearch/UHD.git'
 
   depends_on 'cmake' => :build
   depends_on 'automake' => :build
   depends_on 'libusb'
   depends_on 'boost'
 
+  def patches
+    DATA
+  end
+  
   def install
     cd "host"
     mkdir "build"
@@ -21,7 +25,7 @@ class EttusUhd < Formula
     system "make"
     system "make install" # if this fails, try separate make/make install steps
   end
-
+  
   def test
     # This test will fail and we won't accept that! It's enough to just replace
     # "false" with the main program this formula installs, but it'd be nice if you
@@ -29,3 +33,40 @@ class EttusUhd < Formula
     system "false"
   end
 end
+
+__END__
+diff --git a/host/lib/usrp/dboard_id.cpp b/host/lib/usrp/dboard_id.cpp
+index 3028d2a..24d1f95 100644
+--- a/host/lib/usrp/dboard_id.cpp
++++ b/host/lib/usrp/dboard_id.cpp
+@@ -51,7 +51,10 @@ template <class T> struct to_hex{
+ 
+ dboard_id_t dboard_id_t::from_string(const std::string &string){
+     if (string.substr(0, 2) == "0x"){
+-        return dboard_id_t::from_uint16(boost::lexical_cast<to_hex<boost::uint16_t> >(string));
++	std::stringstream interpreter(string);
++        to_hex<boost::uint16_t> hh;
++        interpreter >> hh;
++        return dboard_id_t::from_uint16(hh);
+     }
+     return dboard_id_t::from_uint16(boost::lexical_cast<boost::uint16_t>(string));
+ }
+diff --git a/host/utils/b2xx_fx3_utils.cpp b/host/utils/b2xx_fx3_utils.cpp
+index c182548..6c5a35d 100644
+--- a/host/utils/b2xx_fx3_utils.cpp
++++ b/host/utils/b2xx_fx3_utils.cpp
+@@ -58,7 +58,10 @@ template <class T> struct to_hex{
+ //!parse hex-formatted ASCII text into an int
+ boost::uint16_t atoh(const std::string &string){
+     if (string.substr(0, 2) == "0x"){
+-        return boost::lexical_cast<to_hex<boost::uint16_t> >(string);
++    	std::stringstream interpreter(string);
++        to_hex<boost::uint16_t> hh;
++        interpreter >> hh;
++        return hh.value;
+     }
+     return boost::lexical_cast<boost::uint16_t>(string);
+ }
+
+
+
