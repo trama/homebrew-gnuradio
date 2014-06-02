8b47a00731871582951e14fa35f3979cf3bca8a7
Federico <fedetx@gmail.com>
2014-06-02 17:05:07 +0200

    quasi done!

--- ettus-uhd.rb
+++ ettus-uhd.rb
@@ -2,9 +2,9 @@ require 'formula'
 
 class EttusUhd < Formula
   homepage 'http://www.ettus.com/'
-  url 'https://github.com/EttusResearch/uhd/archive/release_003_006_001.tar.gz'
-  sha1 'fc67e61a8b962f142ff208865a1dd503e25b7e0b'
-  version '3.6.1'
+  url 'https://github.com/EttusResearch/uhd/archive/release_003_007_001.zip'
+  sha1 '3c5fa141e2f00ed9683e15195904facf1d02de62'
+  version '3.7.1'
   head 'git://github.com/EttusResearch/UHD.git'
 
   depends_on 'cmake' => :build
@@ -12,12 +12,14 @@ class EttusUhd < Formula
   depends_on 'libusb'
   depends_on 'boost'
 
-  def patches
-    DATA
-  end
+  #def patches
+  #  DATA
+  #end
   
   def install
-    cd "host"
+    ENV.prepend_create_path 'PYTHONPATH', libexec+'lib/python2.7/site-packages'
+    install_args = [ "setup.py", "install", "--prefix=#{libexec}" ]
+    #cd "host"
     mkdir "build"
     cd "build"
     system "cmake","../","-DCMAKE_INSTALL_PREFIX=#{prefix}",*std_cmake_args
@@ -35,38 +37,38 @@ class EttusUhd < Formula
 end
 
 __END__
-diff --git a/host/lib/usrp/dboard_id.cpp b/host/lib/usrp/dboard_id.cpp
-index 3028d2a..24d1f95 100644
---- a/host/lib/usrp/dboard_id.cpp
-+++ b/host/lib/usrp/dboard_id.cpp
-@@ -51,7 +51,10 @@ template <class T> struct to_hex{
- 
- dboard_id_t dboard_id_t::from_string(const std::string &string){
-     if (string.substr(0, 2) == "0x"){
--        return dboard_id_t::from_uint16(boost::lexical_cast<to_hex<boost::uint16_t> >(string));
-+	std::stringstream interpreter(string);
-+        to_hex<boost::uint16_t> hh;
-+        interpreter >> hh;
-+        return dboard_id_t::from_uint16(hh);
-     }
-     return dboard_id_t::from_uint16(boost::lexical_cast<boost::uint16_t>(string));
- }
-diff --git a/host/utils/b2xx_fx3_utils.cpp b/host/utils/b2xx_fx3_utils.cpp
-index c182548..6c5a35d 100644
---- a/host/utils/b2xx_fx3_utils.cpp
-+++ b/host/utils/b2xx_fx3_utils.cpp
-@@ -58,7 +58,10 @@ template <class T> struct to_hex{
- //!parse hex-formatted ASCII text into an int
- boost::uint16_t atoh(const std::string &string){
-     if (string.substr(0, 2) == "0x"){
--        return boost::lexical_cast<to_hex<boost::uint16_t> >(string);
-+    	std::stringstream interpreter(string);
-+        to_hex<boost::uint16_t> hh;
-+        interpreter >> hh;
-+        return hh.value;
-     }
-     return boost::lexical_cast<boost::uint16_t>(string);
- }
-
-
-
+# diff --git a/host/lib/usrp/dboard_id.cpp b/host/lib/usrp/dboard_id.cpp
+# index 3028d2a..24d1f95 100644
+# --- a/host/lib/usrp/dboard_id.cpp
+# +++ b/host/lib/usrp/dboard_id.cpp
+# @@ -51,7 +51,10 @@ template <class T> struct to_hex{
+#  
+#  dboard_id_t dboard_id_t::from_string(const std::string &string){
+#      if (string.substr(0, 2) == "0x"){
+# -        return dboard_id_t::from_uint16(boost::lexical_cast<to_hex<boost::uint16_t> >(string));
+# +	std::stringstream interpreter(string);
+# +        to_hex<boost::uint16_t> hh;
+# +        interpreter >> hh;
+# +        return dboard_id_t::from_uint16(hh);
+#      }
+#      return dboard_id_t::from_uint16(boost::lexical_cast<boost::uint16_t>(string));
+#  }
+# diff --git a/host/utils/b2xx_fx3_utils.cpp b/host/utils/b2xx_fx3_utils.cpp
+# index c182548..6c5a35d 100644
+# --- a/host/utils/b2xx_fx3_utils.cpp
+# +++ b/host/utils/b2xx_fx3_utils.cpp
+# @@ -58,7 +58,10 @@ template <class T> struct to_hex{
+#  //!parse hex-formatted ASCII text into an int
+#  boost::uint16_t atoh(const std::string &string){
+#      if (string.substr(0, 2) == "0x"){
+# -        return boost::lexical_cast<to_hex<boost::uint16_t> >(string);
+# +    	std::stringstream interpreter(string);
+# +        to_hex<boost::uint16_t> hh;
+# +        interpreter >> hh;
+# +        return hh.value;
+#      }
+#      return boost::lexical_cast<boost::uint16_t>(string);
+#  }
+# 
+# 
+# 
--- gnuradio.rb
+++ gnuradio.rb
@@ -7,30 +7,29 @@ class Gnuradio < Formula
   head 'git://gnuradio.org/gnuradio.git'
 
   depends_on 'cmake' => :build
-  depends_on 'Cheetah' => :python
-  depends_on 'lxml' => :python
-  depends_on 'numpy' => :python
   depends_on 'scipy' => :python
-  depends_on 'matplotlib' => :python
-  depends_on 'python'
   depends_on 'boost'
   depends_on 'fftw'
-  depends_on 'gsl'
-  # depends_on 'ice'
-  depends_on 'orc'
   depends_on 'pygtk'
-  depends_on 'sdl'
   depends_on 'swig'
   depends_on 'wxpython'
-  # depends_on 'gfortran'
-  # depends_on 'suite-sparse'
   depends_on 'cppunit'
-  # depends_on 'libusb'
-  depends_on 'ettus-uhd'
   depends_on 'pyqt' if ARGV.include?('--with-qt')
   depends_on 'pyqwt' if ARGV.include?('--with-qt')
   depends_on 'doxygen' if ARGV.include?('--with-docs')
-
+  # depends_on 'gfortran'
+  # depends_on 'suite-sparse'
+  # depends_on 'libusb'
+  # depends_on 'sdl'
+  # depends_on 'Cheetah' => :python
+  # depends_on 'lxml' => :python
+  # depends_on 'numpy' => :python
+  # depends_on 'matplotlib' => :python
+  # depends_on 'python'
+  # depends_on 'gsl'
+  # depends_on 'ice'
+  # depends_on 'orc'
+  # depends_on 'ettus-uhd'
   
   fails_with :clang do
     build 421
@@ -44,10 +43,6 @@ class Gnuradio < Formula
     ]
   end
   
-  #def patches
-  #  DATA
-  #end
-
   def install
     ENV.prepend_create_path 'PYTHONPATH', libexec+'lib/python2.7/site-packages'
     install_args = [ "setup.py", "install", "--prefix=#{libexec}" ]
@@ -114,38 +109,3 @@ class Gnuradio < Formula
 end
 
 __END__
-# diff --git a/gr-qtgui/swig/CMakeLists.txt b/gr-qtgui/swig/CMakeLists.txt
-# index a1f7024..53bfe18 100644
-# --- a/gr-qtgui/swig/CMakeLists.txt
-# +++ b/gr-qtgui/swig/CMakeLists.txt
-# @@ -37,6 +37,8 @@ set(GR_SWIG_DOC_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/../include)
-#  
-#  set(GR_SWIG_LIBRARIES gnuradio-qtgui)
-#  
-# +target_link_libraries(swig ${qtgui_libs})
-# +
-#  GR_SWIG_MAKE(qtgui_swig qtgui_swig.i)
-#  
-#  GR_SWIG_INSTALL(
-
-# diff --git a/cmake/Modules/FindQwt.cmake b/cmake/Modules/FindQwt.cmake
-# index d3dc7a5..3c417c7 100644
-# --- a/cmake/Modules/FindQwt.cmake
-# +++ b/cmake/Modules/FindQwt.cmake
-# @@ -39,7 +39,7 @@ if(QWT_INCLUDE_DIRS)
-#      QWT_STRING_VERSION REGEX "QWT_VERSION_STR")
-#    string(REGEX MATCH "[0-9]+.[0-9]+.[0-9]+" QWT_VERSION ${QWT_STRING_VERSION})
-#    string(COMPARE LESS ${QWT_VERSION} "5.2.0" QWT_WRONG_VERSION)
-# -  string(COMPARE GREATER ${QWT_VERSION} "6.0.2" QWT_WRONG_VERSION)
-# +  string(COMPARE GREATER ${QWT_VERSION} "6.1.2" QWT_WRONG_VERSION)
-#  
-#    message(STATUS "QWT Version: ${QWT_VERSION}")
-#    if(NOT QWT_WRONG_VERSION)
-# @@ -56,4 +56,4 @@ if(QWT_FOUND)
-#    include ( FindPackageHandleStandardArgs )
-#    find_package_handle_standard_args( Qwt DEFAULT_MSG QWT_LIBRARIES QWT_INCLUDE_DIRS )
-#    MARK_AS_ADVANCED(QWT_LIBRARIES QWT_INCLUDE_DIRS)
-# -endif(QWT_FOUND)
-# \ No newline at end of file
-# +endif(QWT_FOUND)
-
